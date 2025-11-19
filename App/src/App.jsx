e1d38ae18fe1aa2f65b85ba171e0cba62c31a6cc
diines <janadinesh908@gmail.com>
2025-11-19 16:27:43 +0530

    Practice code

---
+++ App/src/App.jsx
@@ -0,0 +1,19 @@
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+function App() {
+  return( // return only one statement
+    <> 
+    <h1>Welcome to React.</h1>
+    <Home />
+    <About />
+    <Contact />
+    <p>Hello how are you?</p>
+    <h3>thank you</h3>
+    </>
+    // or using div tag
+  )
+}
+
+export default App;
+// npm run dev
